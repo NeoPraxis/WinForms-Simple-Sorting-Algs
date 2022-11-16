2f5f1935bbc297d67e97bf8b9890f4353b75f540
Kaleb Currier <currierka@husson.edu>
2022-11-16 12:50:20 -0500

    Added: click event display data method -For loop to grab the data and display it formatted

--- FormSortingAlgorithms.cs
+++ FormSortingAlgorithms.cs
@@ -13,14 +13,40 @@ namespace SortingAlgorithms
 {
     public partial class FormSortingAlgorithms : Form
     {
+        #region data
+        private int[] valueArray = { 57, 63, 40, 13, 74, 99, 21, 8 };
+        #endregion data
+
+
         public FormSortingAlgorithms()
         {
             InitializeComponent();
         }
 
+        #region events
         private void buttonBubbleInLine_Click(object sender, EventArgs e)
         {
-
+            DisplayData(valueArray, listBoxPreSort);
         }
+        #endregion events
+
+        #region methods
+        /// <summary>
+        /// This routine will display the array of data in the list box
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="theListBox"></param>
+        private void DisplayData(int[] data, ListBox theListBox)
+        {
+            theListBox.Items.Clear();
+
+            // index itself, is the value in the array, hence the naming.
+            for (int numsInArray = 0; numsInArray <= data.GetUpperBound(0); numsInArray++)
+            {
+                // format and display each item in the array in the listbox
+                theListBox.Items.Add(String.Format("[{0}] = {1}", numsInArray, data[numsInArray]));
+            }
+        }
+        #endregion methods
     }
 }
